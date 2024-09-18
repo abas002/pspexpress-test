996807d3d326e3694ee7738a8798025f54c019ab
abas002 <abas.goody0098@gmail.com>
2024-09-18 18:10:39 +0330

    feat: add settings page

---
+++ src/app/admin/settings/page.tsx
@@ -0,0 +1,12 @@
+'use client'
+
+const SettingsPage = () => {
+    return (
+        <div className='h-full flex flex-col justify-center items-center gap-8'>
+            <h1 className='font-bold text-6xl'>Settings Page</h1>
+            <p>This page have sensitive Data.</p>
+        </div>
+    )
+}
+
+export default SettingsPage
