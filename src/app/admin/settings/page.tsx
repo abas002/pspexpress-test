ad71d6e09a2e88fe61375bf4b86e936f374fb535
abas002 <abas.goody0098@gmail.com>
2024-09-18 18:18:00 +0330

    fix: use main insteadOf div

--- src/app/admin/settings/page.tsx
+++ src/app/admin/settings/page.tsx
@@ -2,10 +2,10 @@
 
 const SettingsPage = () => {
     return (
-        <div className='h-full flex flex-col justify-center items-center gap-8'>
+        <main className='h-full flex flex-col justify-center items-center gap-8'>
             <h1 className='font-bold text-6xl'>Settings Page</h1>
             <p>This page have sensitive Data.</p>
-        </div>
+        </main>
     )
 }
 
--- src/app/unathorized/page.tsx
+++ src/app/unathorized/page.tsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 const UnathorizedPage = () => {
     return (
-        <div className='h-full flex flex-col justify-center items-center gap-8'>
+        <main className='h-full flex flex-col justify-center items-center gap-8'>
             <h1 className='font-bold text-6xl'>403</h1>
             <p>THis Page is Restricted!</p>
-        </div>
+        </main>
     )
 }
 export default UnathorizedPage
